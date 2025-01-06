2b27eb2ab216246dc40c0f1746cde64d534e309a
Timo Friman <tjf@iki.fi>
2025-01-06 17:35:45 +0200

    2024 day 4 pt 2

--- 2024/src/aoc/day04.clj
+++ 2024/src/aoc/day04.clj
@@ -1,6 +1,7 @@
 (ns aoc.day04)
 
-(def input "resources/day04-example.txt")
+(def input-ex "resources/day04-example.txt")
+(def input-ex-pt2 "resources/day04-example-pt2.txt")
 (def input "resources/day04.txt")
 
 (defn- get-diag [lines line-length start-pos direction]
@@ -36,4 +37,40 @@
   (let [lines (clojure.string/split-lines (slurp input))]
     (part1 lines)))
 
-(p1)
+(prn "p1" (p1))
+
+(defn coords [len]
+  ;; 0 2
+  ;;  1
+  ;; 0 2
+  (for [x (range 0 (- len 2))]
+    [[x (inc x) (+ 2 x)] [(+ 2 x) (inc x) x]]))
+
+;; p2 3x3 where either MAS or SAM is diagonals.
+(defn get-cross [lines]
+  (let [lines3 (partition 3 1 lines)
+        len (count (first lines))
+        coord-list (coords len)]
+    (map (fn [[u m l]]
+           (map (fn [[[c1u c1m c1l] [c2u c2m c2l]]]
+                  #_(prn u b)
+                  (let [w1 (str
+                             (nth u c1u)
+                             (nth m c1m)
+                             (nth l c1l)
+                             )
+                        w2 (str
+                             (nth u c2u)
+                             (nth m c2m)
+                             (nth l c2l)
+                             )]
+                    (into #{} [w1 w2])
+                    )
+                  ) coord-list)
+           ) lines3)))
+
+(prn "p2" (->> (get-cross (clojure.string/split-lines (slurp input)))
+               flatten
+               (filter (fn [x] (clojure.set/superset? #{"MAS" "SAM"} x)))
+               count
+               ))
