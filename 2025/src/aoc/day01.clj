a11f05644995fc4fa8fc64a0ad2b9596a04ff56f
Timo Friman <tjf@iki.fi>
2025-12-13 13:14:08 +0200

    2025 day 1

--- 2025/src/aoc/day01.clj
+++ 2025/src/aoc/day01.clj
@@ -3,9 +3,6 @@
 (def input-example "resources/day01-example.txt")
 (def input "resources/day01.txt")
 
-(def example1 (clojure.string/split-lines (slurp input-example)))
-(def input1 (clojure.string/split-lines (slurp input)))
-
 (defn- parse-dir [dir]
   (-> dir
       (clojure.string/replace-first #"L" "-")
@@ -16,65 +13,70 @@
   (reduce
     (fn [{:keys [counter current]} v]
       (let [new-current (mod (+ current v) 100)
-            new-counter (if (= 0 new-current) (inc counter) counter)
-            ]
+            new-counter (if (= 0 new-current) (inc counter) counter)]
         {:current new-current
          :counter new-counter}
-        )
-
-      )
+        ))
     {:counter 0, :current 50}
-    (map parse-dir lines))
-  )
+    (map parse-dir lines)))
 
 (defn p1 [i]
   (let [lines (clojure.string/split-lines (slurp i))]
     (part1 lines)))
 
-(defn counting-mod- [start add]
-  (let [
-        x (+ start add)
-        modx (mod x 100)
-        totals (- x modx)
-        rounds (/ totals 100)
-        ]
-    {:x      x
-     :modx   modx
-     :totals totals
-     :rounds rounds
-     :result (abs rounds)}
-    ))
-
-(defn make-right
-  "Changes left rotation to be towards right"
-  [n]
-  (if (pos-int? n)
-    n
-    (let [a (mod n 100)
-          ]
-      (+ (* -1 n) (* 2 (- a 50)))
-      )
-    ))
+(defn handle-neg
+  "If neg, add 100 until not neg, count how many needed"
+  [x start]
+  (let [end (mod x 100)
+        res (quot (abs (- x end)) 100)
+        x (if (= 0 start)
+            (dec res)
+            res
+            )]
+    (if (= 0 end)
+      (inc x)
+      x)))
 
 (comment
   (do
-    (assert (= 145 (make-right -155)))
-    (assert (= 155 (make-right 155)))
-    )
+    (assert (= 1 (handle-neg -1 1)))
+    (assert (= 0 (handle-neg -1 0)))
+    (assert (= 1 (handle-neg -99 1)))
+    (assert (= 2 (handle-neg -101 1)))
+    (assert (= 2 (handle-neg -199 1)))
+    (assert (= 1 (handle-neg -199 0)))
+    (assert (= 2 (handle-neg -100 45)))
+    ))
+
+(defn handle-pos [x]
+  (quot x 100))
+
+(comment
+  (do
+    (assert (= 1 (handle-pos 101)))
+    (assert (= 1 (handle-pos 199)))
+    (assert (= 2 (handle-pos 201)))
+    (assert (= 2 (handle-pos 299))))
   )
 
 (defn counting-mod [start add]
-  (let [
-        x (+ start add)
-        q (quot (make-right x) 100)
-        ]
-    (abs q)
-    ))
+  ;; lahtoluku on aina 0-99. siihen lisataan jotain, jos tulos on valilla 1-99, noop
+  (let [new-sum (+ start add)]
+    #_(println "new sum" new-sum)
+    (if (<= 1 new-sum 99)
+      0
+      (if (= 0 new-sum)
+        1
+        (if (neg? new-sum)
+          (handle-neg new-sum start)
+          (handle-pos new-sum)
+          )))))
 
 (comment
   (do
     (assert (= 1 (counting-mod 95 55)))
-    (assert (= 0 (counting-mod 0 -5)))
+    (assert (= 0 (counting-mod 0 -6)))
+    (assert (= 1 (counting-mod 0 100)))
     (assert (= 1 (counting-mod 95 5)))
     (assert (= 1 (counting-mod 5 95)))
     (assert (= 0 (counting-mod 95 -55)))
@@ -99,7 +101,7 @@
       (let [new-current (mod (+ current v) 100)
             new-counter (+ counter (counting-mod current v))
             ]
-        (println new-current " " new-counter)
+        #_(println new-current " " new-counter)
         {:current new-current
          :counter new-counter}
         )
@@ -107,11 +109,12 @@
     {:counter 0, :current 50}
     (map parse-dir lines))
   )
+
 (defn p2 [i]
   (let [lines (clojure.string/split-lines (slurp i))]
     (part2 lines)))
 
 #_(p1 input-example)
-#_(p1 input)
-(p2 input-example)
-#_(p2 input)
+(p1 input)
+#_(p2 input-example)
+(p2 input)
