0939e48f82070e5ce174d7cef77ba801d10b7c1b
Timo Friman <tjf@iki.fi>
2026-01-01 11:09:45 +0200

    2025 day 2 p2 logic implemented, including repetitive pattern detection

--- 2025/src/aoc/day02.clj
+++ 2025/src/aoc/day02.clj
@@ -31,26 +31,38 @@
 
 (p1 input-example)
 
-(defn check-some-dup
-  "If some of the sublists matches, return value"
-  [s]
-  )
+(defn has-repetitive?
+  "Generates all possible partitions for a given string.
+  Logic is straightforward, first 2,3... until length is reached.
+  The result can be given if all partitions are equal.
+  Quite ineffective!"
+  [l]
+  (let [s (str l)
+        len (if (odd? (count s)) (dec (count s)) (count s))
+        half (/ len 2)
+        all-partitions (map #(partition % % "x" s) (range half 0 -1))
+        equal-ones (filter (fn [s] (= 1 (count (into #{} s)))) all-partitions)]
+    ;;(println "half" half "all" all-partitions "equal" equal-ones)
+    (not (empty? equal-ones))))
 
-(defn check-all-repetitions [s]
-  (some check-some-dup (gen-all-partitions s))
-  )
 (comment
   (do
-    (assert (= true (check-all-repetitions "99")))
-    (assert (= true (check-all-repetitions "999")))
-    (assert (= true (check-all-repetitions "2121212121")))
-    (assert (= true (check-all-repetitions "565656")))
+    (assert (has-repetitive? "12341234"))
+    (assert (has-repetitive? "1111"))
+    (assert (has-repetitive? "99"))
+    (assert (not (has-repetitive? "9")))
+    (assert (has-repetitive? "11111"))
+    (assert (not (has-repetitive? "12121")))
+    (assert (has-repetitive? "2121212121"))
     )
-  )
-(defn p2 [inputs]
-  (parse-to-numbers inputs)
+
   )
 
+(defn p2 [inputs]
+  (-> (filter has-repetitive? (parse-to-numbers inputs))
+      (as-> x (apply + x))))
+
+(p2 input)
 (comment
   (assert (= 4174379265 (p2 input-example)))
   )
